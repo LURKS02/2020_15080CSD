9230a799f1668d6f12d7dd2aedf46a416f0daa2e
LURKS02 <thecrooks2@gmail.com>
2020-06-25 19:51:31 +0900

    simpleprint2

---
+++ hello_world/hello_world/simplePrint2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std; // std 공간에 선언된 모든 이름에서 std:: 생략
+
+int main() {
+	cout << "Subject" << endl;
+	cout << "컴퓨터공학부\n";
+	cout << "Major" << endl;
+	return 0;
+}
