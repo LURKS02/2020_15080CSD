159055ccba4f33bbbb5248e42fc683fde80812ab
LURKS02 <thecrooks2@gmail.com>
2020-06-25 20:02:45 +0900

    readnum

---
+++ hello_world/hello_world/readNum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int thisIsANumber;
+	cout << "Enter a number : ";
+	cin >> thisIsANumber;
+	cout << "You entered : " << thisIsANumber << "\n";
+	return 0;
+}
