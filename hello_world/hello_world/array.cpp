1a83318f533263e07b652aec0bb5776a9348190a
LURKS02 <thecrooks2@gmail.com>
2020-06-26 17:01:32 +0900

    array

---
+++ hello_world/hello_world/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score[3] = { 99, 88, 100 };
+	cout << "score[0]狼 林家 : " << &score[0] << endl;
+	cout << "score[1]狼 林家 : " << &score[1] << endl;
+	cout << "score[2]狼 林家 : " << &score[2] << endl;
+	cout << "================================" << endl;
+	for (int i = 0; i < 3; i++) cout << "score[" << i << "]狼 林家 : " << &score[i] << endl;
+	cout << "================================" << endl;
+	cout << "score[0]狼 林家 : " << score << endl;
+	cout << "score[1]狼 林家 : " << score + 1 << endl;
+	cout << "score[2]狼 林家 : " << score + 2 << endl;
+	cout << "================================" << endl;
+	for (int i = 0; i < 3; i++) cout << "score[" << i << "]狼 林家 : " << score + i << endl;
+}
