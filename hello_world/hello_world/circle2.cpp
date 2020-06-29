3b70e0cc366e59608b38a5980d21b8465fdde6f5
LURKS02 <thecrooks2@gmail.com>
2020-06-29 20:00:19 +0900

    destructor

--- hello_world/hello_world/circle2.cpp
+++ hello_world/hello_world/circle2.cpp
@@ -5,6 +5,7 @@ public:
 	int radius;
 	Circle();
 	Circle(int r);
+	~ Circle(); //소멸자
 	double getArea();
 };
 
@@ -16,6 +17,10 @@ Circle::Circle() {
 Circle::Circle(int r) {
 	radius = r;
 	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::~ Circle() {
+	cout << "반지름 " << radius << " 원 소멸" << endl;
 }
 
 double Circle::getArea() {
@@ -30,4 +35,10 @@ int main() {
 	Circle pizza(12);
 	area = pizza.getArea();
 	cout << "pizza 면적은 " << area << endl;
+
+	/*
+	main()함수가 종료하면 스택에 생성된 객체 소멸
+	이때 객체 생성 반대순으로 소멸
+	*/
+	return 0; 
 }
