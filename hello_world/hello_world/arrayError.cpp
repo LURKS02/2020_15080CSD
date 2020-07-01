50c2cecde2ef1d27750dbbc0b54c7d60c62cd98f
LURKS02 <thecrooks2@gmail.com>
2020-07-01 19:53:37 +0900

    add

--- hello_world/hello_world/arrayError.cpp
+++ hello_world/hello_world/arrayError.cpp
@@ -6,6 +6,7 @@ class Circle {
 public:
 	//inline
 	Circle(int r) { radius = r; }
+	void setRadius(int r) { radius = r; }
 	double getArea() { return 3.14 * radius * radius; }
 };
 
@@ -13,4 +14,6 @@ int main() {
 	Circle waffle(15);
 	Circle circleArray[3] = { Circle(10), Circle(20), Circle(30) };
 	//Circle circleArray[3];	기본 생성자가 존재하지 않음
+	for (int i = 0; i < 3; i++)
+		cout << "Circle " << i << "의 면적은 " << circleArray[i].getArea() << endl;
 }
