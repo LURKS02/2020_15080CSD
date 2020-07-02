b64ce9d2f8524d88831cec766a420336a98b5b5f
LURKS02 <thecrooks2@gmail.com>
2020-07-02 20:10:18 +0900

    call by value

---
+++ hello_world/hello_world/callbyvalue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swap(int a, int b) {
+	int tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+
+	cout << "in function : " << a << ' ' << b << endl;
+}
+
+int main() {
+	int m = 2, n = 9;
+	cout << "before(main): " << m << ' ' << n << endl;
+	swap(m, n);
+	cout << "after(main): " << m << ' ' << n << endl;
+}
